43300f719e2704f6de1b4cd8e47acaf6defcdfd8
Patryk Kowalski <patrykkowalski0617@gmail.com>
2022-08-12 15:27:21 +0200

    init and products mock

---
+++ src/components/ProductsList/ProductsList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import productsData from "../../data/productsData";
+
+const ProductsList = () => {
+  return (
+    <>
+      {productsData.map(({ imgName, brand, name, price }) => (
+        <div>
+          <img src={require(`../../assets/img/${imgName}`)} />
+          <p>
+            {brand} - {name}
+          </p>
+          <p>{price}</p>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default ProductsList;
